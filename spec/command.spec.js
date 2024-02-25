5e999cb71378b88a79ab946b7f7ff88fe149b582
Nicole Wade <nwade0916@gmail.com.com>
2024-02-25 16:44:16 -0600

    Graded assignment 3 6 passed

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,15 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  // Test 2
+  test("constructor sets command type", function() {
+    let command = new Command('MOVE', 100);
+    expect(command.commandType).toEqual('MOVE');
+  });
+  // Test 3: 
+  test("constructor sets a value passed in as the second argument", function() {
+    let command = new Command('MOVE', 100);
+    expect(command.value).toEqual(100);
+  });
 
 });
